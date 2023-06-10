86ab42487ab3cd9ce11d5389d00e40cfadb6e6a2
RafiTahmid <rafi.tahmid019@gmail.com>
2023-06-10 11:20:56 +0600

    property-binding, two-way binding and strng interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+  myTitle: string;
+  imageSrc: string;
+  favoriteAnimal: string
+
+  constructor() {
+    this.myTitle = "Learning Angular";
+    this.imageSrc = "https://th.bing.com/th/id/OIP.2bJ9_f9aKoGCME7ZIff-ZwHaJ4?pid=ImgDet&rs=1"
+    this.favoriteAnimal = "Cat"
+  }
 }
